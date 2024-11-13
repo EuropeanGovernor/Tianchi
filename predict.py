3deb8e09403f58c7e3e5facd79fdc7ebbbc7945a
.y. <71976926+EuropeanGovernor@users.noreply.github.com>
2024-11-14 00:09:28 +0800

    Delete predict.py

--- predict.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.run(['llamafactory-cli train ../LLaMA-Factory/examples/predict.yaml'])
-subprocess.run(['cp -r ../LLaMA-Factor/saves/qwen2_vl-7b/lora/sft-infer ../MIRE/data/demo_pred.jsonl'])
-subprocess.run(['python ../MIRE/mire_baseline/convert2submit.py'])
-subprocess.run(['python ../MIRE/mire_baseline/cal_acc.py'])
